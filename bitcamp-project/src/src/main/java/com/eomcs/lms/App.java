a2ae943078179380b6bd657f45ec75e6c6bc36fd
leejiwon <wldnjs2516@naver.com>
2019-12-10 18:05:18 +0900

    유기 동물 관리 시스템

---
+++ bitcamp-project/src/src/main/java/com/eomcs/lms/App.java
@@ -0,0 +1,13 @@
+package src.main.java.com.eomcs.lms;
+
+public class App {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("번호: 1");
+    System.out.println("내용: 게시글입니다.");
+    System.out.println("작성일: 2019-01-01");
+    System.out.println("조회수: 0");
+  }
+
+}
