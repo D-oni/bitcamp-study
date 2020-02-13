2d9fe9ca5cce4fb08b33db3dfcb63dc3d17e36b1
leejiwon <wldnjs2516@naver.com>
2020-02-13 17:00:05 +0900

    프로젝트 스레드 적용

--- bitcamp-project-client/src/main/java/com/eomcs/lms/dao/proxy/BoardDaoProxy.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/dao/proxy/BoardDaoProxy.java
@@ -6,74 +6,105 @@ import java.util.List;
 import com.eomcs.lms.dao.BoardDao;
 import com.eomcs.lms.domain.Board;
 
+// 프록시 객체는 항상 작업 객체와 동일한 인터페이스를 구현해야 한다.
+// => 마치 자신이 작업 객체인양 보이기 위함이다.
+//
 public class BoardDaoProxy implements BoardDao {
 
-  ObjectInputStream in;
-  ObjectOutputStream out;
+  DaoProxyHelper daoProxyHelper;
 
-  public BoardDaoProxy(ObjectInputStream in, ObjectOutputStream out) {
-    this.in=in;
-    this.out=out;
+  public BoardDaoProxy(DaoProxyHelper daoProxyHelper) {
+    this.daoProxyHelper=daoProxyHelper;
   }
 
   @Override
   public int insert(Board board) throws Exception {
-      out.writeUTF("/board/add");
-      out.writeObject(board);
-      out.flush();
+    class InsertWorker implements Worker{
+      @Override
+      public Object execute(ObjectInputStream in , ObjectOutputStream out) throws Exception {
+        out.writeUTF("/board/add");
+        out.writeObject(board);
+        out.flush();
 
-      String response = in.readUTF();
-      if (response.equals("FAIL")) {
-        throw new Exception(in.readUTF());
+        String response = in.readUTF();
+        if (response.equals("FAIL")) {
+          throw new Exception(in.readUTF());
+        }
+
+        return 1;
       }
-
-      return 1;
     }
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<Board> findAll() throws Exception {
-      out.writeUTF("/board/list");
+    InsertWorker worker = new InsertWorker();
+    int resultState = (int)daoProxyHelper.request(worker);
+    return resultState;
+  }
 
-      out.flush();
+  @SuppressWarnings("unchecked")
+  @Override
+  public List<Board> findAll() throws Exception {
 
-      String response = in.readUTF();
-      if (response.equals("FAIL")) {
-        throw new Exception(in.readUTF());
+    Worker worker = new Worker() {
+      @Override
+      public Object execute(ObjectInputStream in, ObjectOutputStream out) throws Exception {
+
+        out.writeUTF("/board/list");
+        out.flush();
+        String response = in.readUTF();
+        if (response.equals("FAIL")) {
+          throw new Exception(in.readUTF());
+        }
+        return in.readObject();
       }
-        return (List<Board>) in.readObject();
+    };
+    Object result = daoProxyHelper.request(worker);
+    return (List<Board>)result;
+
     }
 
-    @Override
-    public Board findByNo(int no) throws Exception {
-      out.writeUTF("/board/detail");
-      out.writeInt(no);
-      out.flush();
+  @Override
+  public Board findByNo(int no) throws Exception {
+    Object result = daoProxyHelper.request( new Worker() {
+      @Override
+      public Object execute(ObjectInputStream in, ObjectOutputStream out) throws Exception {
 
-      String response = in.readUTF();
+        out.writeUTF("/board/detail");
+        out.writeInt(no);
+        out.flush();
 
-      if (response.equals("FAIL")) {
-        System.out.println(in.readUTF());
-        throw new Exception(in.readUTF());
+        String response = in.readUTF();
+        if (response.equals("FAIL")) {
+          throw new Exception(in.readUTF());
+        }
+        return in.readObject();
+
       }
-      return (Board) in.readObject();
-    }
+    });
+    return (Board)result;
 
-    @Override
-    public int update(Board board) throws Exception {
-      out.writeUTF("/board/update");
-      out.writeObject(board);
-      out.flush();
+  }
 
-      String response = in.readUTF();
-      if (response.equals("FAIL")) {
-        System.out.println(in.readUTF());
+  @Override
+  public int update(Board board) throws Exception {
+    return (int) daoProxyHelper.request( new Worker() {
+      @Override
+      public Object execute(ObjectInputStream in, ObjectOutputStream out) throws Exception {
+
+        out.writeUTF("/board/update");
+        out.writeObject(board);
+        out.flush();
+
+        String response = in.readUTF();
+        if (response.equals("FAIL")) {
+          throw new Exception(in.readUTF());
+        }
+        return 1;
       }
-      return 1;
-    }
-
-    @Override
-    public int delete(int no) throws Exception {
+    });
+  }
+  @Override
+  public int delete(int no) throws Exception {
+    return (int) daoProxyHelper.request(( in,  out)-> {
       out.writeUTF("/board/delete");
       out.writeInt(no);
       out.flush();
@@ -83,5 +114,6 @@ public class BoardDaoProxy implements BoardDao {
         throw new Exception(in.readUTF());
       }
       return 1;
-    }
+    });
   }
+}
--- bitcamp-project-client/src/main/java/com/eomcs/lms/dao/proxy/MemberDaoProxy.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/dao/proxy/MemberDaoProxy.java
@@ -1,23 +1,25 @@
 package com.eomcs.lms.dao.proxy;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.List;
 import com.eomcs.lms.dao.MemberDao;
 import com.eomcs.lms.domain.Member;
 
+// 프록시 객체는 항상 작업 객체와 동일한 인터페이스를 구현해야 한다.
+// => 마치 자신이 작업 객체인양 보이기 위함이다.
+// => MemberDao 작업 객체를 대행할 프록시를 정의한다.
+//
 public class MemberDaoProxy implements MemberDao {
 
-  ObjectInputStream in;
-  ObjectOutputStream out;
 
-  public MemberDaoProxy(ObjectInputStream in, ObjectOutputStream out) {
-    this.in=in;
-    this.out=out;
+  DaoProxyHelper daoProxyHelper;
+
+  public MemberDaoProxy(DaoProxyHelper daoProxyHelper) {
+    this.daoProxyHelper=daoProxyHelper;
   }
 
   @Override
   public int insert(Member member) throws Exception {
+    return (int)  daoProxyHelper.request((in,out) -> {
       out.writeUTF("/member/add");
       out.writeObject(member);
       out.flush();
@@ -26,54 +28,57 @@ public class MemberDaoProxy implements MemberDao {
       if (response.equals("FAIL")) {
         throw new Exception(in.readUTF());
       }
-
       return 1;
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<Member> findAll() throws Exception {
+    });
+  }
+  @SuppressWarnings("unchecked")
+  @Override
+  public List<Member> findAll() throws Exception {
+    return (List<Member>)daoProxyHelper.request((in,out) -> {
       out.writeUTF("/member/list");
-
       out.flush();
-
       String response = in.readUTF();
       if (response.equals("FAIL")) {
         throw new Exception(in.readUTF());
       }
-        return (List<Member>) in.readObject();
-    }
+      return (List<Member>) in.readObject();
+    });
+  }
 
-    @Override
-    public Member findByNo(int no) throws Exception {
+
+  @Override
+  public Member findByNo(int no) throws Exception {
+    return (Member)daoProxyHelper.request((in,out) -> {
       out.writeUTF("/member/detail");
       out.writeInt(no);
       out.flush();
 
       String response = in.readUTF();
-
       if (response.equals("FAIL")) {
-        System.out.println(in.readUTF());
         throw new Exception(in.readUTF());
       }
       return (Member) in.readObject();
-    }
+    });
+  }
 
-    @Override
-    public int update(Member member) throws Exception {
+  @Override
+  public int update(Member member) throws Exception {
+    return (int)daoProxyHelper.request((in,out) -> {
       out.writeUTF("/member/update");
       out.writeObject(member);
       out.flush();
 
       String response = in.readUTF();
       if (response.equals("FAIL")) {
-        System.out.println(in.readUTF());
+        throw new Exception(in.readUTF());
       }
       return 1;
-    }
+    });
+  }
 
-    @Override
-    public int delete(int no) throws Exception {
+  @Override
+  public int delete(int no) throws Exception {
+    return (int)daoProxyHelper.request((in,out) -> {
       out.writeUTF("/member/delete");
       out.writeInt(no);
       out.flush();
@@ -83,5 +88,7 @@ public class MemberDaoProxy implements MemberDao {
         throw new Exception(in.readUTF());
       }
       return 1;
-    }
+    });
   }
+
+}
