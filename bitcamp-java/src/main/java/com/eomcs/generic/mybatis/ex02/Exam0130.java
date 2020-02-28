ab844fc6c79553ede71a4bb77fc6aa945bbfa57c
user <wldnjs2516@naver.com>
2020-02-28 18:30:00 +0900

    2020-02-28

--- bitcamp-java/src/main/java/com/eomcs/generic/mybatis/ex02/Exam0130.java
+++ bitcamp-java/src/main/java/com/eomcs/generic/mybatis/ex02/Exam0130.java
@@ -1,4 +1,4 @@
-// Mybatis - SQL에 파라미터 지정하기 : ${}의 쓰임새
+// Mybatis - 자바 객체의 프로퍼티 이름과 컬럼명을 일치시키기 II
 package com.eomcs.mybatis.ex02;
 
 import java.io.InputStream;
@@ -8,28 +8,25 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class Exam0221 {
+public class Exam0130 {
 
   public static void main(String[] args) throws Exception {
     InputStream inputStream = Resources.getResourceAsStream(//
-        "com/eomcs/mybatis/ex02/mybatis-config05.xml");
+        "com/eomcs/mybatis/ex02/mybatis-config03.xml");
     SqlSessionFactory factory = //
         new SqlSessionFactoryBuilder().build(inputStream);
 
     SqlSession sqlSession = factory.openSession();
 
-    //예) 파라미터로 컬럼 이름을 넘겨주면
-    //해당 컬럼의 값을 오름차순으로 정렬한다.
-
-
-    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard2", "created_date");
-    // ${} 문법은 값을 SQL문에 그대로 삽입한다.
-
-
+    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard");
 
     for (Board board : list) {
-      System.out.printf("%d, %s, %s, %s\n", board.getNo(), board.getTitle(), board.getContent(),
-          board.getRegisteredDate());
+      System.out.printf("%d, %s, %s, %s, %d\n", //
+          board.getNo(), //
+          board.getTitle(), //
+          board.getContent(), //
+          board.getRegisteredDate(), //
+          board.getViewCount());
     }
 
     sqlSession.close();
--- bitcamp-java/src/main/java/com/eomcs/generic/mybatis/ex02/Exam0221.java
+++ bitcamp-java/src/main/java/com/eomcs/generic/mybatis/ex02/Exam0221.java
@@ -1,4 +1,4 @@
-// Mybatis - SQL에 파라미터 지정하기 : #{} 의 한계
+// Mybatis - SQL에 파라미터 지정하기 : ${}의 쓰임새
 package com.eomcs.mybatis.ex02;
 
 import java.io.InputStream;
@@ -8,7 +8,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class Exam0220 {
+public class Exam0221 {
 
   public static void main(String[] args) throws Exception {
     InputStream inputStream = Resources.getResourceAsStream(//
@@ -18,21 +18,17 @@ public class Exam0220 {
 
     SqlSession sqlSession = factory.openSession();
 
-    //예) 파라미터로 컬럼 이름을 넘겨주면
-    //해당 컬럼의 값을 오름차순으로 정렬한다.
-
-
-    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard3", "title");
-    //파라미터 값을 SQL에 그대로 삽입하려면
-    // #{} 문법을 사용해서는 안된다.
-    // ${} 문법을 사용해야한다.
-    // => #{} 은 값을 넣을 때 사용
-    // => 현재 selectBoard1에서는 #{}을 사용했기때문에
-    // => order by 가 제대로 적용되지 못했다.
-
+    // 예) 파라미터로 컬럼 이름을 넘겨주면
+    // 해당 컬럼의 값을 오름차순으로 정렬한다.
+    List<Board> list = sqlSession.selectList(//
+        "BoardMapper.selectBoard2", "title");
+    // ${} 문법은 값을 SQL 문에 그대로 삽입한다.
 
     for (Board board : list) {
-      System.out.printf("%d, %s, %s, %s\n", board.getNo(), board.getTitle(), board.getContent(),
+      System.out.printf("%d, %s, %s, %s\n", //
+          board.getNo(), //
+          board.getTitle(), //
+          board.getContent(), //
           board.getRegisteredDate());
     }
 
